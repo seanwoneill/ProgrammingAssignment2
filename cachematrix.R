d71d4a34a16921338d261ab042ef79746a9afa70
S W ONeill <seanwoneill@gmail.com>
2015-01-24 11:20:45 -0500

    Cache matrix inverse function completed
    Final function which takes a matrix and (1) calculates its inverse if
    the inverse has not already been calculated, or (2) returns the inverse
    matrix from memory if it has previously been calculated.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The functions below determine the inverse of a supplied
+# matrix if this inverse matric has not already been
+# calcualted.  If already calculated, the inverse matrix
+# is pulled from memory.
 
-## Write a short comment describing this function
 
+# makeCacheMatrix creates a special "matrix", which
+# is really a list containing a function to: (1) set 
+# the value of the matrix, (2) get the value of the 
+# matrix, (3) set the value of the inverse matrix, 
+# and (4) get the value of the inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve calculates the inverse of the special "matrix" 
+# created with makeCacheMatrix. However, it first checks to 
+# see if the inverse has already been calculated. If so, it 
+# gets the inverse from the cache and skips the computation. 
+# Otherwise, it calculates the inverse of the matrix and sets 
+# the value of the inverse in the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
